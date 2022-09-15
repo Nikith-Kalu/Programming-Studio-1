bca60bb34c89d806c280522ba1b7f99f164978e4
Nikith Kalu <nikithkalu@gmail.com>
2022-09-15 15:39:29 +1000

    Update WordFrequencies.java

--- WRK-Week01/java/WordFrequencies.java
+++ WRK-Week01/java/WordFrequencies.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class WordFrequencies {
    public static void main(String[] args) {      
+
       // STEP 1: Read the number of words into an integer
+      Scanner scnr = new Scanner(System.in);
+      ArrayList<String> list = new ArrayList<String>();
+      int num = scnr.nextInt();
 
       // STEP 2: Read the words, one-at-a-time into an ArrayList
-
+      for (int i = 0; i < num; i++) {
+         list.add(scnr.next());
+      }
       // STEP 3: Find the number of times the *first* word appears in the ArrayList.
       //         Print out this number in the required format.
-
+      
       // STEP 4: Question - What do you need to change in your Step 3 code
       //                    to find the number of times the *second* word
       //                    appears in the ArrayList?
@@ -16,6 +23,6 @@ public class WordFrequencies {
       //         Question - Do you see a pattern emerging?
 
       // STEP 5: Turn your code in Step 3 into a loop that checks every word in the ArrayList
-
+      }
    }
 }
