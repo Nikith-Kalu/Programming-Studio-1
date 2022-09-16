578e4d2f0acc868241cbe1e0adba2e7b68fca68d
Nikith Kalu <nikithkalu@gmail.com>
2022-09-16 11:16:22 +1000

    Update Listing.java

--- cosc2803-week02/java/Listing.java
+++ cosc2803-week02/java/Listing.java
@@ -4,9 +4,65 @@ public class Listing {
    private double price;
 
    // TODO: Add more properties
+   private String brand;
+   private String description;
+   private double origPrice;
+   private double discPrice;
 
-   // TODO: Add your setter/getter here
-   
+   // Title
+   public String getTitle() {
+      return title; 
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   // Price
+   public String getPrice() {
+      return price; 
+   }
+
+   public void setPrice(double price) {
+      this.price = price;
+   }
+
+   // Brand
+   public String getBrand() {
+      return brand; 
+   }
+
+   public void setBrand(String brand) {
+      this.brand = brand;
+   }
+
+   // Description
+   public String getDescription() {
+      return description; 
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   // Original Price
+   public double getOrigPrice() {
+      return origPrice; 
+   }
+
+   public void setOrigPrice(double origPrice) {
+      this.origPrice = origPrice;
+   }
+
+   // Discounted Price
+   public double getDiscPrice() {
+      return discPrice; 
+   }
+
+   public void setDiscPrice(double discPrice) {
+      this.discPrice = discPrice ;
+   }
+
    public void printInfo() {
       // TODO: Complete this to print out the information of the Listing
       System.out.println("Item: " + title);
